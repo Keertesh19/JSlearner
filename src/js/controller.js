3b9778938cf7d9ba35728d1a9749714ad18acec5
Keertesh <keertesh.k98@gmail.com>
2021-08-05 22:24:52 +0530

    Initial JS push

---
+++ src/js/controller.js
@@ -0,0 +1,82 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    // Render Spinner
+    recipeView.renderSpinner();
+
+    resultsView.update(model.getSearchResultsPage());
+
+    // 1) Loading recipes
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    const query = await searchView.getQuery();
+    if (!query) return;
+
+    // Render Spinner
+    resultsView.renderSpinner();
+
+    // 1) Loading search results
+    await model.loadSearchResults(query);
+
+    // 2) Rendering resultss
+    resultsView.render(model.getSearchResultsPage());
+
+    // 3) Render Page View
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const controlPagination = function (gotoPage) {
+  resultsView.render(model.getSearchResultsPage(gotoPage));
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  model.updateServings(newServings);
+
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.removeBookmark(model.state.recipe.id);
+
+  recipeView.update(model.state.recipe);
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+
+  controlSearchResults();
+};
+init();
---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,6 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class BookmarksView extends View {
+  _parentElement = document.querySelector('.bookmarks');
+}
